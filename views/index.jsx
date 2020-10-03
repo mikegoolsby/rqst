2eb04cea1f6101139b8c8497e41abc66a82c277a
Michael Goolsby <mgoolsby1091@gmail.com>
2020-10-03 13:12:05 -0400

    refactors index

--- views/index.jsx
+++ views/index.jsx
@@ -13,32 +13,31 @@ class Index extends React.Component {
         return (
             <Layout>
                 <h2 class="d-flex justify-content-center">Approved Requests</h2>
-                {request.map((request, index) => {
-                let isPending = request.pending
-                if (isPending === false) {
-                    return (
-                        
                 <div class="d-flex align-items-center">
                 <table class="table table-striped table-hover table-sm align-items-center tablesorter" id="grid" data-toggle="bootgrid" data-ajax="true" data-url="/api/data/basic" class="table table-condensed table-hover table-striped">
                     <thead class="thead-dark">
                         <tr>
-                        <th scope="col" data-column-id="date">Date</th>
+                        <th scope="col" data-column-id="date" onClick="sortTable(0)">Date</th>
                         <th scope="col" data-column-id="Name">Name</th>
                         <th scope="col" data-column-id="Bucket">Bucket</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <th scope="row" class="table-active">{request.date}</th>
-                        <td>{request.name}</td>
-                        <td>{request.bucket}</td>
-                        </tr>
+                        {request.map((request, index) => {
+                        let isPending = request.pending
+                        if (isPending === false) {
+                            return (
+                                <tr>
+                                <th scope="row" class="table-active">{request.date}</th>
+                                <td>{request.name}</td>
+                                <td>{request.bucket}</td>
+                                </tr>
+                                )
+                            }
+                            })}
                     </tbody>
                 </table>
                 </div>
-                    )
-                }
-                })}
             </Layout>
         )
     }
